243e5969f29d9114f8d13559d7e2eb7d1936ea3a
Ademayowa adeniyi <ademayowaadeniyi@gmail.com>
2020-04-07 10:21:00 +0100

    renamed testimonials for testing purposes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,7 +3,7 @@ import Hero from './Hero';
 import Banner from './Banner';
 import About from './About';
 import Services from './Services';
-import Testimonials from './Testimonials';
+import Testimonials from './Testimonials1';
 import Footer from '../layout/Footer';
 import Blog from './Blog';
 
